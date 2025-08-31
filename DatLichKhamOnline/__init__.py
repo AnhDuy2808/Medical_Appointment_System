850a2069a56bf6834da07d90587fc1ac2f00fca5
hiennhatou <2251012058hien@ou.edu.vn>
2025-08-31 16:26:00 +0700

    fix: flask-login

--- DatLichKhamOnline/__init__.py
+++ DatLichKhamOnline/__init__.py
@@ -2,13 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import cloudinary
 import cloudinary.uploader
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.secret_key = "@%%@@^$!@$%%#@%%@PHAD%@$#$&*&"
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:Hien388@localhost/mas?charset=utf8mb4"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
-db = SQLAlchemy(app = app)
+login = LoginManager(app=app)
+
+db = SQLAlchemy(app=app)
 
 cloudinary.config(cloud_name='du6rdcpyi', api_key='943655164841773', api_secret='NlZzlTXX4fVHVswderpmViCM3oY')
 
